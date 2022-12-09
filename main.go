0fa368dda168e5adba22f6ff5aa0e20b4ce48572
arpan-ti <108259323+arpan-ti@users.noreply.github.com>
2022-12-09 20:44:15 +0530

    commit (#5)

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func readReccosFile(fileName string) (map[string]map[string][]string, error) {
 	reccosMap := map[string]map[string][]string{}
 	file, err := os.Open(fileName)
 	if err != nil {
-		return reccosMap, err //System fail
+		return reccosMap, err
 	}
 
 	scanner := bufio.NewScanner(file)
@@ -25,18 +25,18 @@ func readReccosFile(fileName string) (map[string]map[string][]string, error) {
 	for scanner.Scan() {
 		line := scanner.Text()
 		items := strings.Split(line, ":") //items[0] -> AWSID, items[1] -> attributeType items[2] -> attributeValue
-		if len(items)<2 {
+		if len(items) < 2 {
 			return reccosMap, errors.New("Corrupt Recommendation")
 		}
 		innerMap, exists := reccosMap[items[0]]
 		if !exists {
 			tempMap := make(map[string][]string) // a new map will have to be made as a map with the given AWSID does not exist
-			for i:=2; i<len(items); i++ {
+			for i := 2; i < len(items); i++ {
 				tempMap[items[1]] = append(tempMap[items[1]], items[i])
 			}
 			reccosMap[items[0]] = tempMap
 		} else {
-			for i:=2; i<len(items); i++ {
+			for i := 2; i < len(items); i++ {
 				innerMap[items[1]] = append(innerMap[items[1]], items[i])
 			}
 			reccosMap[items[0]] = innerMap
@@ -64,15 +64,14 @@ func readTagFile(fileName string) (map[string]string, error) {
 
 func main() {
 	reccosFileName := os.Getenv("ReccosMapFile")
-	tagFileName := os.Getenv("TagsMapFile") //both of these environment variables would have been set by the orchestrator
+	tagFileName := os.Getenv("TagsMapFile")
 	currPWDStrip := ""
 	reccosFilePath := ""
 	tagFilePath := ""
 	if runtime.GOOS == "windows" {
 		currPWD, err := exec.Command("powershell", "-NoProfile", "(pwd).path").Output()
 		if err != nil {
-			//Add error log
-			panic(err) //system crash
+			panic(err)
 		}
 		currPWDStrip = strings.Trim(string(currPWD), "\n")
 		currPWDStrip = strings.TrimSuffix(currPWDStrip, "\r")
@@ -81,25 +80,20 @@ func main() {
 	} else {
 		currPWD, err := exec.Command("pwd").Output()
 		if err != nil {
-			//Add error log
-			panic(err) //system crash
+			panic(err)
 		}
 		currPWDStrip = strings.Trim(string(currPWD), "\n")
 		reccosFilePath = currPWDStrip + "/" + reccosFileName
 		tagFilePath = currPWDStrip + "/" + tagFileName
 	}
-	//there is a new line char by defualt that needs to be trimmed
-
 	reccos, errR := readReccosFile(reccosFilePath)
 	if errR != nil {
-		//Add error log
-		panic(errR) //system fail
+		panic(errR)
 	}
 
 	tagToID, errT := readTagFile(tagFilePath)
 	if errT != nil {
-		//Add err log
-		panic(errT) //system fail
+		panic(errT)
 	}
 	var taggableMap = make(map[string]bool)
 	for _, resourceType := range taggableArray {
